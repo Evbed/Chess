2072c251861133f94f7c70eab9f12ffc5694d30c
Evgeny Bedarev <evgeny.bedarev@hypercell.games>
2024-03-18 15:58:26 +0300

    Revert "Create project"
    This reverts commit bcbe8622e8750da63731135eab5c38186be24798.

--- Assets/Scripts/FigureColor.cs
+++
@@ -1,8 +0,0 @@
-namespace DefaultNamespace
-{
-	public enum FigureColor
-	{
-		White,
-		Black
-	}
-}
--- Assets/Scripts/Rules.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Rules : MonoBehaviour
-{
-    public bool IsPossibleMove(Board board, Vector2 from, Vector2 to)
-    {
-        return true;
-    }
-}
