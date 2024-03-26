042eca35c430e1753626d53af7cde906359e3121
Evgeny Bedarev <Evbed@mail.ru>
2024-03-26 18:10:44 +0300

    Pawn diagonal moves cases

--- Assets/Scripts/Core/RuleSet/Figures/PawnAvailableMoves.cs
+++ Assets/Scripts/Core/RuleSet/Figures/PawnAvailableMoves.cs
@@ -9,9 +9,25 @@ namespace Core.RuleSet.Figures
         public IReadOnlyCollection<Vector2Int> MovesFor(Vector2Int coordinate, IBoard board)
         {
             List<Vector2Int> directions = new List<Vector2Int>();
+            Vector2Int diagonalRight = new Vector2Int(coordinate.x + 1, coordinate.y + 1);
+            Vector2Int diagonalLeft = new Vector2Int(coordinate.x -1, coordinate.y + 1);
 
-            directions.Add(new Vector2Int(coordinate.x, coordinate.y + 2)); //TODO only for fist move
+            if (coordinate.y == 1 || coordinate.y == 6) // start positions
+            {
+                directions.Add(new Vector2Int(coordinate.x, coordinate.y + 2));
+            }
+            
             directions.Add(new Vector2Int(coordinate.x, coordinate.y + 1));
+            
+            if (board.Cell(diagonalRight).Figure() != null)
+            {
+                directions.Add(diagonalRight);
+            }
+            
+            if (board.Cell(diagonalLeft).Figure() != null)
+            {
+                directions.Add(diagonalLeft);
+            }
             
             return directions;
         }
