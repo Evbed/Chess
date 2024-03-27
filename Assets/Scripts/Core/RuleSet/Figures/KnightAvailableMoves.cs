1d75b8a43ac76cbcaab8da47398a3cc47b7b171d
Evgeny Bedarev <evgeny.bedarev@hypercell.games>
2024-03-27 17:47:23 +0300

    Knight rotate and mirror original move direction

--- Assets/Scripts/Core/RuleSet/Figures/KnightAvailableMoves.cs
+++ Assets/Scripts/Core/RuleSet/Figures/KnightAvailableMoves.cs
@@ -9,29 +9,41 @@ namespace Core.RuleSet.Figures
         public IReadOnlyCollection<Vector2Int> MovesFor(Vector2Int coordinate, IBoard board)
         {
             List<Vector2Int> directions = new List<Vector2Int>();
-            
-            Vector2Int upRight = new Vector2Int(1, 2);
-            Vector2Int upLeft = new Vector2Int(-1, 2);
-            
-            Vector2Int rightUp = new Vector2Int(2, 1);
-            Vector2Int rightDown = new Vector2Int(2, -1);
-            
-            Vector2Int downRight = new Vector2Int(1, -2);
-            Vector2Int downLeft = new Vector2Int(-1, -2);
-            
-            Vector2Int leftUp = new Vector2Int(-2, 1);
-            Vector2Int leftDown = new Vector2Int(-2, -1);
-            
-            directions.Add(upRight);
-            directions.Add(upLeft);
-            directions.Add(rightUp);
-            directions.Add(rightDown);
-            directions.Add(downRight);
-            directions.Add(downLeft);
-            directions.Add(leftUp);
-            directions.Add(leftDown);
+         
+            Vector2Int move = new Vector2Int(1, 2);
+            directions.Add(move);
 
+            foreach (var m in RotateAndMirror(move))
+            {
+                directions.Add(m);
+            }
+            
             return directions;
+        }
+        
+        private List<Vector2Int> RotateAndMirror(Vector2Int vector)
+        {
+            List<Vector2Int> rotationsAndMirror = new List<Vector2Int>();
+            
+            //mirror original move
+            rotationsAndMirror.Add(new Vector2Int(-vector.x, -vector.y));
+
+            //90 deg
+            Vector2Int rotated90 = new Vector2Int(vector.y, -vector.x);
+            rotationsAndMirror.Add(rotated90);
+            rotationsAndMirror.Add(new Vector2Int(-rotated90.x, rotated90.y)); 
+
+            //180 deg
+            Vector2Int rotated180 = new Vector2Int(-vector.x, -vector.y);
+            rotationsAndMirror.Add(rotated180);
+            rotationsAndMirror.Add(new Vector2Int(-rotated180.x, rotated180.y)); 
+
+            //270 deg
+            Vector2Int rotated270 = new Vector2Int(-vector.y, vector.x);
+            rotationsAndMirror.Add(rotated270);
+            rotationsAndMirror.Add(new Vector2Int(-rotated270.x, rotated270.y)); 
+
+            return rotationsAndMirror;
         }
     }
 }
