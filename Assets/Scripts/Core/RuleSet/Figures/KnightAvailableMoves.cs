c267a84477576d04c65fc6c111d5c334e2fe3f04
Evgeny Bedarev <evgeny.bedarev@hypercell.games>
2024-03-27 15:51:41 +0300

    Knight available moves

--- Assets/Scripts/Core/RuleSet/Figures/KnightAvailableMoves.cs
+++ Assets/Scripts/Core/RuleSet/Figures/KnightAvailableMoves.cs
@@ -6,10 +6,32 @@ namespace Core.RuleSet.Figures
 {
     public class KnightAvailableMoves : IAvailableMoves
     {
-
         public IReadOnlyCollection<Vector2Int> MovesFor(Vector2Int coordinate, IBoard board)
         {
-            throw new System.NotImplementedException();
+            List<Vector2Int> directions = new List<Vector2Int>();
+            
+            Vector2Int upRight = new Vector2Int(1, 2);
+            Vector2Int upLeft = new Vector2Int(-1, 2);
+            
+            Vector2Int rightUp = new Vector2Int(2, 1);
+            Vector2Int rightDown = new Vector2Int(2, -1);
+            
+            Vector2Int downRight = new Vector2Int(1, -2);
+            Vector2Int downLeft = new Vector2Int(-1, -2);
+            
+            Vector2Int leftUp = new Vector2Int(-2, 1);
+            Vector2Int leftDown = new Vector2Int(-2, -1);
+            
+            directions.Add(upRight);
+            directions.Add(upLeft);
+            directions.Add(rightUp);
+            directions.Add(rightDown);
+            directions.Add(downRight);
+            directions.Add(downLeft);
+            directions.Add(leftUp);
+            directions.Add(leftDown);
+
+            return directions;
         }
     }
 }
